3eed12c947f7a8ee0190b8016d83c6293a1c782d
Mihir Thanekar <mihirthanekar@gmail.com>
2019-03-30 10:44:52 -0700

    TODO: change to pdf value

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ const pdf = require('pdf-parse');
 
 let dataBuffer = fs.readFileSync('testbank.pdf');
 
-var userFile = document.getElementById("upload").value;
+var userFile = document.getElementById("upload").value; // this is btn value not pdf value.
+//TODO: CHange to pdf value
+console.log(userFile)
 
 pdf(dataBuffer).then(function(data){
 	console.log(data.numpages);
