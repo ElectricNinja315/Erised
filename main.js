f50cf192589cd1dd4de57622c37f68d0b62bf6b0
Mihir Thanekar <mihirthanekar@gmail.com>
2019-03-30 10:58:57 -0700

    upload pdf store

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 const fs = require('fs');
 const pdf = require('pdf-parse');
 
+
+function onUpload() {
+	var userFile = document.getElementById("file").files; // this is btn value not pdf value.
+	//TODO: CHange to pdf value
+	console.log(userFile)
+}
+
+
+
+
 let dataBuffer = fs.readFileSync('testbank.pdf');
-
-var userFile = document.getElementById("upload").value; // this is btn value not pdf value.
-//TODO: CHange to pdf value
-console.log(userFile)
-
 pdf(dataBuffer).then(function(data){
 	console.log(data.numpages);
 	console.log(data.info);
