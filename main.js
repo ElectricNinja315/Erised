398d85f1897a36e7c08a96007d5f15174c3352c8
Mihir Thanekar <mihirthanekar@gmail.com>
2019-03-30 09:27:45 -0700

    pdf

---
+++ main.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const pdf = require('pdf-parse');
+
+let dataBuffer = fs.readFileSync('');
+
+pdf(dataBuffer).then(function(data){
+	console.log(data.numpages);
+	console.log(data.info);
+	console.log(data.text);
+
+})
