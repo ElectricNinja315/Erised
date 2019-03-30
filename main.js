95f049a4ba78cc9ed5084ba1950c159190cff454
Mihir Thanekar <mihirthanekar@gmail.com>
2019-03-30 12:12:13 -0700

    not working pdf readfile

--- main.js
+++ main.js
@@ -1,20 +1,21 @@
 const fs = require('fs');
 const pdf = require('pdf-parse');
 
+console.log(fs)
 
 function onUpload() {
 	var userFile = document.getElementById("file").files; // this is btn value not pdf value.
 	//TODO: CHange to pdf value
 	console.log(userFile)
-}
-
-
-
-
-let dataBuffer = fs.readFileSync('testbank.pdf');
+	let dataBuffer = fs.readFileSync('testbank.pdf');
 pdf(dataBuffer).then(function(data){
 	console.log(data.numpages);
 	console.log(data.info);
 	console.log(data.text);
 
 })
+}
+
+
+
+
