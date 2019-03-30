0e6237a0500ca407710e6bc68861719f68723e73
ElectricNinja315 <suzac314@gmail.com>
2019-03-30 10:37:29 -0700

    add get element

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const pdf = require('pdf-parse');
 
 let dataBuffer = fs.readFileSync('testbank.pdf');
 
+var userFile = document.getElementById("upload").value;
+
 pdf(dataBuffer).then(function(data){
 	console.log(data.numpages);
 	console.log(data.info);
